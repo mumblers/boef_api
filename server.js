41717053a3adeb6383647177f251359918057db2
Phochs <me.accounts+github@runbox.com>
2018-05-27 00:24:42 +0200

    Started on a new map!

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(logger);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 /*
  * Root page
